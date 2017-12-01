b86ee82fcb11c3a1e3b66a2bb40fd5f6d0b945f6
Reid Wilbur <reid.wilbur@gmail.com>
2017-12-01 13:16:23 -0500

    Fixing comment

--- src/main/java/com/wilb0t/aoc/Day1.java
+++ src/main/java/com/wilb0t/aoc/Day1.java
@@ -6,7 +6,7 @@ public class Day1 {
     int sum = 0;
     for(int i = 0; i < digits.length(); i++) {
       if (digits.charAt(i) == digits.charAt((i + 1) % digits.length())) {
-        // utf8/asci char 48 is digit 0
+        // utf8/ascii char 48 is digit 0
         sum += digits.charAt(i) - 48;
       }
     }
@@ -17,7 +17,7 @@ public class Day1 {
     int sum = 0;
     for(int i = 0; i < digits.length(); i++) {
       if (digits.charAt(i) == digits.charAt((i + (digits.length()/2)) % digits.length())) {
-        // utf8/asci char 48 is digit 0
+        // utf8/ascii char 48 is digit 0
         sum += digits.charAt(i) - 48;
       }
     }
